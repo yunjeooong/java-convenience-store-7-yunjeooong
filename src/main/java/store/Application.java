fb02e5f9703fcd642601041d3b62679a9e73f8af
yunjeooong <yunjeongssu@gmail.com>
2024-11-10 03:03:59 +0900

    refactor(MembershipDiscountPolicy): 메서드 분리를 통한 가독성 개선

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -13,7 +13,5 @@ public class Application {
 
         mainController.start();
         orderController.processOrder();
-
-
     }
 }
--- src/main/java/store/domain/discount/MembershipDiscountPolicy.java
+++ src/main/java/store/domain/discount/MembershipDiscountPolicy.java
@@ -6,19 +6,20 @@ import store.domain.vo.Money;
 public class MembershipDiscountPolicy implements DiscountPolicy {
     private static final double DISCOUNT_RATE = 0.3;
     private static final int MAX_DISCOUNT = 8000;
-
     @Override
     public Money calculateDiscount(Order order) {
         if (!order.hasMembership()) {
             return Money.ZERO;
         }
-
-        Money nonPromotionAmount = order.calculateNonPromotionAmount();
-        Money membershipDiscount = new Money((int) (nonPromotionAmount.value() * DISCOUNT_RATE));
-
-        if (membershipDiscount.value() > MAX_DISCOUNT) {
+        Money discount = calculateMembershipDiscount(order);
+        if (discount.value() > MAX_DISCOUNT) {
             return new Money(MAX_DISCOUNT);
         }
-        return membershipDiscount;
+        return discount;
+    }
+
+    private Money calculateMembershipDiscount(Order order) {
+        Money nonPromotionAmount = order.calculateNonPromotionAmount();
+        return new Money((int) (nonPromotionAmount.value() * DISCOUNT_RATE));
     }
 }
